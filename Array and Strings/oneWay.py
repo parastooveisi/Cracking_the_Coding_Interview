438ebaa6515db83ab4d22ab9f2b233532dc0b11c
parastooveisi <parastoo.veisi@gmail.com>
2020-11-16 20:25:53 -0600

    Implement One Edit Away function

---
+++ Array and Strings/oneWay.py
@@ -0,0 +1,21 @@
+def oneWay(s1, s2):
+    hashMap1 = {}
+    hashMap2 = {}
+    count = 0
+
+    for char in s1:
+        hashMap1[char] = s1.count(char)
+
+    for char in s2:
+        hashMap2[char] = s2.count(char)
+
+    for key in hashMap1:
+        if key not in hashMap2:
+            count += 1
+    return count == 1 or count == 0
+
+
+print(oneWay("pale", "ple"))  # True
+print(oneWay("pales", "pale"))  # True
+print(oneWay("pale", "bale"))  # True
+print(oneWay("pale", "bake"))  # False
