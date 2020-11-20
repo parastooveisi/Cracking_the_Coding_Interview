bce0fa42c9f25905331260d1dab456999359c308
parastooveisi <parastoo.veisi@gmail.com>
2020-11-19 19:22:33 -0600

    Implement remove duplicates in a linked list

---
+++ Linked Lists/removeDuplicates.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        curr = self.head
+        while curr:
+            print(curr.data)
+            curr = curr.next
+
+    def append(self, data):
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+            return
+
+        lastNode = self.head
+        while lastNode.next:
+            lastNode = lastNode.next
+
+        lastNode.next = newNode
+
+    def removeDuplicates(self):
+        curr = self.head
+        prev = None
+        hashMap = dict()
+
+        while curr:
+            if curr.data in hashMap:
+                prev.next = curr.next
+                curr = None
+            else:
+                hashMap[curr.data] = 1
+                prev = curr
+            curr = prev.next
+
+
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(3)
+llist.removeDuplicates()
+llist.printList()
