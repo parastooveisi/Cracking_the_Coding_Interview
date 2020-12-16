10cb242401361940bb4a6c6b93247f41934ffd8d
parastooveisi <parastoo.veisi@gmail.com>
2020-12-16 13:17:31 -0600

    Rename the files

---
+++ Circular Linked List/01_circular_linkedList.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        curr = self.head
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+            return
+
+        while curr.next != self.head:
+            curr = curr.next
+
+        curr.next = new_node
+        new_node.next = self.head
+
+    def print_list(self):
+        curr = self.head
+
+        while curr:
+            print(curr.data)
+            curr = curr.next
+            if curr == self.head:
+                break
+
+    def prepend(self, data):
+        new_node = Node(data)
+        if not self.head:
+            new_node.next = new_node
+            return
+
+        last = self.head
+        while last.next != self.head:
+            last = last.next
+
+        last.next = new_node
+        new_node.next = self.head
+        self.head = new_node
+
+
+c = CircularLinkedList()
+c.append("A")
+c.append("B")
+c.prepend("C")
+c.print_list()
---
+++ Circular Linked List/03_remove_node.py
@@ -0,0 +1,67 @@
+# A -> B -> C -> D -> ...
+# remove key = "B"
+# A -> C -> D -> ...
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CircularList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = self.head
+            return
+
+        curr = self.head
+        while curr.next != self.head:
+            curr = curr.next
+
+        curr.next = new_node
+        new_node.next = self.head
+
+    def printList(self):
+        curr = self.head
+        while curr:
+            print(curr.data)
+            curr = curr.next
+            if curr == self.head:
+                break
+
+    def remove(self, key):
+        curr = self.head
+        if self.head.data == key:
+            while curr.next != self.head:
+                curr = curr.next
+
+            curr.next = self.head.next
+            self.head = self.head.next
+            return
+
+        prev = None
+        while curr.data != key:
+            prev = curr
+            curr = curr.next
+
+        prev.next = curr.next
+        curr = None
+
+
+clist = CircularList()
+clist.append("A")
+clist.append("B")
+clist.append("C")
+
+clist.printList()
+
+clist.remove("B")
+
+clist.remove("C")
+clist.printList()
