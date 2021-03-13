99a1472ec31ee876ff37a078dbaf58894e496978
parastooveisi <parastoo.veisi@gmail.com>
2021-03-13 10:46:27 -0600

    Change logic a bit

--- Trees and Graphs/bfs_dfs.py
+++ Trees and Graphs/bfs_dfs.py
@@ -2,10 +2,11 @@
 
 
 def dfs(graph, node, visited):
-    if node not in visited:
-        visited.append(node)
-        for k in graph[node]:
-            dfs(graph, k, visited)
+    if node in visited:
+        return
+    visited.append(node)
+    for k in graph[node]:
+        dfs(graph, k, visited)
     return visited
 
 
