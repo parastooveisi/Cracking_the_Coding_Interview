b52b4321d14fd7fbdd10dcf22a120d922506bcc8
parastooveisi <parastoo.veisi@gmail.com>
2021-03-13 10:48:38 -0600

    Remove unneccesary function

--- Trees and Graphs/minimal_tree.py
+++ Trees and Graphs/minimal_tree.py
@@ -7,19 +7,10 @@ def convert_to_tree(inputarr):
 
     mid = len(inputarr) // 2
     root = Node(inputarr[mid])
+    print(root.data)
     root.left = convert_to_tree(inputarr[:mid])
     root.right = convert_to_tree(inputarr[mid+1:])
     return root
 
 
-def preOrder(root):
-    if not root:
-        return
-
-    print(root.data)
-    preOrder(root.left)
-    preOrder(root.right)
-
-
-root = convert_to_tree([1, 2, 3, 4, 5, 6, 7])
-preOrder(root)
+convert_to_tree([1, 2, 3, 4, 5, 6, 7])
